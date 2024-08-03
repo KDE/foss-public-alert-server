47b2fdfa3a5c40d06fa2954e6de82dce35db6998
Nucleus <nucleus-ffm@posteo.de>
2024-08-03 20:28:13 +0200

    use etags

--- foss_public_alert_server/alertHandler/DWD_CAP_parser.py
+++ foss_public_alert_server/alertHandler/DWD_CAP_parser.py
@@ -1,19 +1,34 @@
-import datetime
 import io
 import requests
 import zipfile
 
 from .abstract_CAP_parser import AbstractCAPParser
+from sourceFeedHandler.models import CAPFeedSource
 
 
 class DWDCAPParser(AbstractCAPParser):
 
     def __init__(self, feed_source):
-        super().__init__(feed_source)
+        super().__init__(feed_source, "dwd_parser")
 
-    def get_feed(self):
-        feed_data = requests.get(self.feed_source.cap_alert_feed)
-        zip_file = zipfile.ZipFile(io.BytesIO(feed_data.content), 'r')
+    def _load_alerts_from_feed(self):
+        # use last e tag to reduce network usage
+        headers = {
+        "ETag": self.feed_source.last_e_tag,  # what if None?
+        }
+        response = requests.get(self.feed_source.cap_alert_feed, headers=headers)  # @todo why not cached?
+        if response.status_code == 304:
+            # print(f"Nothing changed for {self.feed_source.source_id}")
+            return
+        elif response.status_code != 200:
+            raise "Feed status code is not 200"
+
+        # update etag and store it in the database
+        new_etag = response.headers.get("ETag")
+        # print(f"etag is {new_etag}")
+        CAPFeedSource.objects.filter(id=self.feed_source.id).update(last_e_tag=new_etag)
+
+        zip_file = zipfile.ZipFile(io.BytesIO(response.content), 'r')
         for cap_file in zip_file.namelist():
             cap_data = zip_file.read(cap_file).decode('utf-8')
-            self.addAlert(cap_data = cap_data)
+            self.addAlert(cap_data=cap_data)
