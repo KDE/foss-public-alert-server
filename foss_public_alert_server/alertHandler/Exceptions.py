d34cfda8e204bac07a148ba45229f4c738ad8512
Nucleus <nucleus-ffm@posteo.de>
2024-08-03 20:33:14 +0200

    add custom exceptions

---
+++ foss_public_alert_server/alertHandler/Exceptions.py
@@ -0,0 +1,14 @@
+class AlertExpiredException(BaseException):
+    pass
+
+
+class DatabaseWritingException(BaseException):
+    pass
+
+
+class AlertParameterException(BaseException):
+    pass
+
+
+class NoGeographicDataAvailableException(BaseException):
+    pass
