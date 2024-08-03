f5a46ed1f3c709c247d2d4ef31fe13a55d873d70
Nucleus <nucleus-ffm@posteo.de>
2024-08-03 20:47:15 +0200

    add sources url and add debug url pattern to get media content with the debug server

--- foss_public_alert_server/foss_public_alert_server/urls.py
+++ foss_public_alert_server/foss_public_alert_server/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('subscription/', include("subscriptionHandler.urls")),
-    path('alert/', include("alertHandler.urls"))
+    path('alert/', include("alertHandler.urls")),
+    path('sources/', include("sourceFeedHandler.urls"))
 ]
+
+# needed to allow media access with the development server
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
