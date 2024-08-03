77f60107557bf00325f8dcdeaaec9863d40266c5
Nucleus <nucleus-ffm@posteo.de>
2024-08-03 20:27:47 +0200

    use etags and improve error handling

--- foss_public_alert_server/alertHandler/XML_CAP_parser.py
+++ foss_public_alert_server/alertHandler/XML_CAP_parser.py
@@ -1,64 +1,83 @@
 import datetime
 import requests
 
-from .abstract_CAP_parser import AbstractCAPParser
 import feedparser
+from feedparser import FeedParserDict
+
 from django.conf import settings
+
+from .abstract_CAP_parser import AbstractCAPParser
+from sourceFeedHandler.models import CAPFeedSource
 
 
 class XMLCAPParser(AbstractCAPParser):
 
     def __init__(self, feed_source):
-        super().__init__(feed_source)
+        super().__init__(feed_source, "xml_parser")
         self.parser = feedparser
 
-    def get_feed(self):
-        try:
-            print("fetching: " + self.feed_source.cap_alert_feed)
-            feed = self.parser.parse(self.feed_source.cap_alert_feed)
+    def _load_alerts_from_feed(self):
+        print("fetching: " + self.feed_source.source_id)
+        # use etags to reduce network load
+        last_e_tag = self.feed_source.last_e_tag
+        # the etag can be none, so only use it, if it is not none
+        if last_e_tag is not None:
+            feed: FeedParserDict = self.parser.parse(self.feed_source.cap_alert_feed, etag=last_e_tag) # @todo change user agent
+        else:
+            feed: FeedParserDict = self.parser.parse(self.feed_source.cap_alert_feed)
 
-            #print("got: " + str(feed))
+        if feed.status == 304:
+            # print(f"Nothing changed for {self.feed_source.source_id}")
+            return
+        elif feed.status != 200:
+            raise "Feed status code is not 200"
 
-            for entry in feed['entries']:
-                print("Found Entry")
-                # print(entry)
-                # find the link to the CAP source
-                cap_source_url = ''
-                for link in entry['links']:
-                    if link['type'] == 'application/cap+xml':  # may also application/rss+xml
-                        cap_source_url = link['href']
-                if not cap_source_url and len(entry['links']) == 1:
-                    cap_source_url = entry['links'][0]['href']
-                if not cap_source_url:
+        # check if header has en etag and if yes update the etag in the database
+        if "etag" in feed.headers:
+            new_etag = feed.headers["etag"]
+            # print(f"etag is {new_etag}")
+            CAPFeedSource.objects.filter(id=self.feed_source.id).update(last_e_tag=new_etag)
+        else:
+            # print("feed does not have an etag")
+            pass
+
+        # print("got: " + str(feed))
+
+        for entry in feed['entries']:
+            # print("Found Entry")
+            # print(entry)
+            # find the link to the CAP source
+            cap_source_url = ''
+            for link in entry['links']:
+                if link['type'] == 'application/cap+xml':  # may also application/rss+xml
+                    cap_source_url = link['href']
+            if not cap_source_url and len(entry['links']) == 1:
+                cap_source_url = entry['links'][0]['href']
+            if not cap_source_url:
+                continue
+
+            # if we have expiry data available here already, check that
+            # to avoid additional downloads
+            try:
+                expire_time = datetime.datetime.fromisoformat(entry.get('cap_expires'))
+                if expire_time is not None and expire_time < datetime.datetime.now(datetime.timezone.utc):
+                    print(f"Alert Expired: {self.feed_source.source_id} - not downloading alert {cap_source_url} expired on {expire_time} - skipping")
                     continue
+            except ValueError as e:
+                print(f"Failed to parse expiry time: {entry.get('cap_expires')}")
+            except TypeError as e:
+                pass
 
-                # if we have expiry data available here already, check that
-                # to avoid additional downloads
-                try:
-                    expire_time = datetime.datetime.fromisoformat(entry.get('cap_expires'))
-                    if expire_time is not None and expire_time < datetime.datetime.now(datetime.timezone.utc):
-                        print(f"{self.source} - not downloading alert {cap_source_url} expired on {expire_time}")
-                        continue
-                except ValueError as e:
-                    print(f"Failed to parse expiry time: {entry.get('cap_expires')}")
-                except TypeError as e:
-                    pass
-
-                try:
-                    req = self.session.get(cap_source_url, headers={'User-Agent': settings.USER_AGENT})
-                    if not req.ok:
-                        print(f"Fetch error {req.status_code}: {cap_source_url}")
-                        continue
-                except requests.exceptions.ConnectionError as e:
-                    print(f"Connection error: {cap_source_url}")
+            try:
+                req = self.session.get(cap_source_url, headers={'User-Agent': settings.USER_AGENT})
+                if not req.ok:
+                    print(f"Fetch error {req.status_code}: {cap_source_url}")
                     continue
+            except requests.exceptions.ConnectionError as e:
+                print(f"Connection error: {cap_source_url}")
+                continue
 
-                cap_data = req.content.decode('utf-8')
-                print("got feed, call addAlert")
-                self.addAlert(cap_source_url=cap_source_url, cap_data=cap_data)
-
-        except Exception as e:
-            print("Something went wrong while getting the Feed: " + str(e))
-            # @todo add to error logger
-
-        print("got feed")
+            cap_data = req.content.decode('utf-8')
+            # print("got feed, call addAlert")
+            # add alert to database
+            self.addAlert(cap_source_url=cap_source_url, cap_data=cap_data)
