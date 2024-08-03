f2e10413d5e667f71a188610c79ba35ab5c29d93
Nucleus <nucleus-ffm@posteo.de>
2024-08-03 20:37:01 +0200

    add some status attributes and an etag field, delete the periodic task automatically with the database entry

--- foss_public_alert_server/sourceFeedHandler/models.py
+++ foss_public_alert_server/sourceFeedHandler/models.py
@@ -1,11 +1,9 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 from django.db import models
 import uuid
+
+from django.dispatch import receiver
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
-from alertHandler.DWD_CAP_parser import DWDCAPParser
 from django.conf import settings
 
 
@@ -16,7 +14,7 @@ class CAPFeedSource(models.Model):
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     source_id = models.CharField(max_length=255)
-    code = models.CharField(max_length=255) # @todo there can be more then one entry here
+    code = models.CharField(max_length=255)  # @todo there can be more then one entry here
     name = models.CharField(max_length=255)
     logo = models.CharField(max_length=255, null=True)
     guid = models.CharField(max_length=255, null=True)
@@ -31,25 +29,43 @@ class CAPFeedSource(models.Model):
     ignore = models.BooleanField(null=True)
 
     last_fetch_status = models.BooleanField(null=True)
-
+    last_fetch_duration = models.DurationField(null=True)
+    missing_geo_information = models.BooleanField(null=True)
+    feed_warnings = models.CharField(max_length=255, null=True)
+    last_e_tag = models.CharField(max_length=255, null=True)
+    periodic_task_name = models.CharField(max_length=255, null=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.create_periodic_task()
+        # create periodic tasks for every feeds that is not ignored and operating
+        if not self.ignore and self.cap_alert_feed_status == "operating":
+            self.create_periodic_task()
 
-    def create_periodic_task(self):
-        if self.format == "DWD-Zip":
-            parser = DWDCAPParser(self)
-            schedule = self.get_or_create_interval()
+    def create_periodic_task(self) -> None:
+        """
+        create a periodic task for this feed source
+        :return: None
+        """
+        schedule = self.get_or_create_interval()
+        task_name = f'periodic feed updater for - {self.source_id} - {self.id}'
+        # only create task if it not already exists and if the feed is not set to ignore
+        if not PeriodicTask.objects.filter(name=task_name).exists():
             task = PeriodicTask.objects.create(
                 interval=schedule,
-                name=f'Periodic task for - {self.id}',
-                task=parser.get_feed(),
+                name=task_name,
+                task='task.create_parser_and_get_feed',
+                args=f'["{self.id}", "{self.format}"]',
             )
             task.save()
+            self.periodic_task_name = task_name
 
-
-    def get_or_create_interval(self):
+    @staticmethod
+    def get_or_create_interval():
+        """
+        get or create an interval for the periodic scheduler
+        uses settings.DEFAULT_UPDATE_PERIOD_FOR_CAP_FEEDS as period
+        :return:
+        """
         schedule, created = IntervalSchedule.objects.get_or_create(
             every=settings.DEFAULT_UPDATE_PERIOD_FOR_CAP_FEEDS,
             period=IntervalSchedule.SECONDS,
@@ -57,4 +73,13 @@ class CAPFeedSource(models.Model):
         return schedule
 
 
-
+@receiver(models.signals.post_delete, sender=CAPFeedSource)
+def auto_delete_periodic_task_on_delete(sender, instance:CAPFeedSource, **kwargs) -> None:
+    """
+    called if CAPFeedSource entry is deleted. Deletes also the periodic task
+    :param instance: The actual instance being deleted.
+    :param kwargs:
+    :return: None
+    """
+    if PeriodicTask.objects.filter(name=instance.periodic_task_name).exists():
+        PeriodicTask.objects.filter(name=instance.periodic_task_name).delete()
