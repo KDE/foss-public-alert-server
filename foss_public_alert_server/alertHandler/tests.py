3538b10c9ec087478fdab683382e2b944d2cf2fb
Volker Krause <vkrause@kde.org>
2024-12-05 18:17:11 +0100

    Fix bounding box validity check
    Adds basic test which failed before, and will be useful in the next step
    of also correctly computing the bounding boxes for circle areas.

--- foss_public_alert_server/alertHandler/tests.py
+++ foss_public_alert_server/alertHandler/tests.py
@@ -20,13 +20,13 @@ class AlertHandlerCAPParserTestsCase(TestCase):
         return XMLCAPParser(feed)
 
     @staticmethod
-    def create_test_cap_data():
-        f = open(os.path.join(settings.BASE_DIR, 'alertHandler/test_data/test_cap_data_1.xml'))
+    def create_test_cap_data(testFile):
+        f = open(os.path.join(settings.BASE_DIR, 'alertHandler/test_data', testFile))
         return f.read()
 
     # test every helper methode
     def test_find_identifier(self):
-        cap_data = self.create_test_cap_data()
+        cap_data = self.create_test_cap_data('test_cap_data_1.xml')
         abstract_cap_parser = self.create_test_class_instance()
 
         ET.register_namespace('', 'urn:oasis:names:tc:emergency:cap:1.2')
@@ -39,3 +39,18 @@ class AlertHandlerCAPParserTestsCase(TestCase):
 
         cap_id = abstract_cap_parser.find_identifier(cap_tree)
         self.assertEquals(cap_id, "urn:oid:1234.5678")
+
+    # test circle bounding box computation
+    def test_circle_area(self):
+        cap_data = self.create_test_cap_data('circle-area.xml')
+        abstract_cap_parser = self.create_test_class_instance()
+
+        ET.register_namespace('', 'urn:oasis:names:tc:emergency:cap:1.2')
+        try:
+            cap_tree = ET.fromstring(cap_data)
+        except ET.ParseError as e:
+            print(f"failed to parse CAP alert message XML: {e}")
+            return
+
+        (min_lat, min_lon, max_lat, max_lon) = abstract_cap_parser.determine_bounding_box(cap_tree, 'dummy')
+        self.assertTrue(AbstractCAPParser.is_valid_bounding_box(min_lon=min_lon, min_lat=min_lat, max_lat=max_lat, max_lon=max_lon))
