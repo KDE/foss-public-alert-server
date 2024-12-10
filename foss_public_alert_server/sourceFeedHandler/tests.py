8a65501789089b6b46c0863a3ba75dba87f0ead9
Nucleus <nucleus-ffm@posteo.de>
2024-12-10 10:08:54 +0100

    update dataset to include latest_published_alert_datetime

--- foss_public_alert_server/sourceFeedHandler/tests.py
+++ foss_public_alert_server/sourceFeedHandler/tests.py
@@ -23,15 +23,19 @@ class SourceFeedHandlerTestsCase(TestCase):
 
         expected_response = (b'{"results": [{"name": "Deutschland: Landeshochwasserportal", "source_is_official": '
                              b'true, "cap_alert_feed_status": "operating", "authorityCountry": "de", "register_url": '
-                             b'null}, {"name": "Deutschland: Bundesamt f\\u00fcr Bev\\u00f6lkerungschutz und '
+                             b'null, "latest_published_alert_datetime": null}, {"name": "Deutschland: Biwapp", '
+                             b'"source_is_official": true, "cap_alert_feed_status": "operating", "authorityCountry": '
+                             b'"de", "register_url": null, "latest_published_alert_datetime": null}, '
+                             b'{"name": "Deutschland: Bundesamt f\\u00fcr Bev\\u00f6lkerungschutz und '
                              b'Katastrophenhilfe", "source_is_official": true, "cap_alert_feed_status": "operating", '
-                             b'"authorityCountry": "de", "register_url": null}, {"name": "Deutschland: Biwapp", '
+                             b'"authorityCountry": "de", "register_url": null, "latest_published_alert_datetime": '
+                             b'"2024-12-10T06:34:18Z"}, {"name": "Deutschland: Deutscher Wetterdienst", '
                              b'"source_is_official": true, "cap_alert_feed_status": "operating", "authorityCountry": '
-                             b'"de", "register_url": null}, {"name": "Deutschland: Deutscher Wetterdienst", '
-                             b'"source_is_official": true, "cap_alert_feed_status": "operating", "authorityCountry": '
-                             b'"de", "register_url": null}, {"name": "France: Meteo-France", "source_is_official": '
-                             b'true, "cap_alert_feed_status": "operating", "authorityCountry": "fr", "register_url": '
-                             b'"https://alertingauthority.wmo.int/authorities.php?recId=53"}]}')
+                             b'"de", "register_url": null, "latest_published_alert_datetime": null}, '
+                             b'{"name": "France: Meteo-France", "source_is_official": true, "cap_alert_feed_status": '
+                             b'"operating", "authorityCountry": "fr", "register_url": '
+                             b'"https://alertingauthority.wmo.int/authorities.php?recId=53", '
+                             b'"latest_published_alert_datetime": "2024-12-10T05:00:44Z"}]}')
 
         self.assertEqual(response.content, expected_response)
 
