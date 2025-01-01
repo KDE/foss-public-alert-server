79e4bde92bcd82a0690543c54eb53dd6cbd5ce96
Volker Krause <vkrause@kde.org>
2025-01-01 12:12:19 +0100

    Show the subscription bounding box properly in the admin interface
    Same as done for alert bounding boxes, using the OSM map now.

--- foss_public_alert_server/subscriptionHandler/admin.py
+++ foss_public_alert_server/subscriptionHandler/admin.py
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from django.contrib import admin
+from django.contrib.gis import admin
 from .models import Subscription
 
+class SubscriptionAdmin(admin.GISModelAdmin):
+    pass
+
 # Register your models here.
-admin.site.register(Subscription)
+admin.site.register(Subscription, SubscriptionAdmin)
