8ab21931c455c00acb051d7eed316f6659b66bda
Volker Krause <vkrause@kde.org>
2025-02-09 15:27:34 +0100

    Add admin action to purge dangling CAP files
    That is CAP files without a corresponding entry in the database.
    
    In theory those shouldn't exist, practically we have about twice as many
    CAP files than alerts on the server.

--- foss_public_alert_server/sourceFeedHandler/admin.py
+++ foss_public_alert_server/sourceFeedHandler/admin.py
@@ -1,12 +1,43 @@
 # SPDX-FileCopyrightText: Nucleus <nucleus-ffm@posteo.de>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
 from .models import CAPFeedSource
-# Register your models here.
+from alertHandler.models import Alert
+
+import glob
+import os
+
 
 class CAPSourceAdmin(admin.ModelAdmin):
     list_display = ('id', 'source_id', 'name')
     search_fields = ['source_id']
+    actions = ['purge_dangling_cap_files']
+
+    @admin.action(description="Purge dangling CAP files")
+    def purge_dangling_cap_files(self, request, queryset):
+        count = 0
+        errors = 0
+        for source in queryset:
+            alert_files = []
+            for alert in Alert.objects.filter(source_id=source.source_id):
+                alert_files.append(alert.cap_data.path)
+
+            to_delete = []
+            for cap_file in glob.glob(f"{settings.MEDIA_ROOT}/alerts/{source.source_id}/*.xml"):
+                if cap_file not in alert_files:
+                    to_delete.append(cap_file)
+
+            for file_name in to_delete:
+                try:
+                    os.remove(file_name)
+                    count += 1
+                except Exception as e:
+                    print(f"Failed to delete {file_name} - {str(e)}")
+                    errors += 1
+
+        self.message_user(request, f"Removed {count} file(s), {errors} error(s)", messages.SUCCESS if errors == 0 else messages.ERROR)
+
 
 admin.site.register(CAPFeedSource, CAPSourceAdmin)
