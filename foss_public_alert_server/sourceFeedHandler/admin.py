85b7c22b44848235e3e6e6645ad8d6701c0faff3
Nucleus <nucleus-ffm@posteo.de>
2025-02-11 17:58:17 +0100

    allow to search for cap feeds

--- foss_public_alert_server/sourceFeedHandler/admin.py
+++ foss_public_alert_server/sourceFeedHandler/admin.py
@@ -7,6 +7,6 @@ from .models import CAPFeedSource
 
 class CAPSourceAdmin(admin.ModelAdmin):
     list_display = ('id', 'source_id', 'name')
-    search_fields = ['source_id']
+    search_fields = ['source_id', 'cap_alert_feed']
 
 admin.site.register(CAPFeedSource, CAPSourceAdmin)
