93eecc3dd7264966415218cfd741dcb396491885
apple cuckoo <nufjoysb@duck.com>
2025-04-12 21:41:17 +1200

    SAME autofetch

--- foss_public_alert_server/alertHandler/generate_SAME.py
+++ foss_public_alert_server/alertHandler/generate_SAME.py
@@ -1,21 +1,22 @@
 # SPDX-FileCopyrightText: 2025 Nucleus <nucleus-ffm@posteo.de>
+# SPDX-FileCopyrightText: 2025 applecuckoo <nufjoysb@duck.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import json
 import os
+import shapefile
 
-# download the shapefile from https://www.weather.gov/gis/NWRPartialCounties (data in public domain)
-# extract the geojson e.g. with QGIS - Desktop and run this script to separate each code
-# in the future we should find a solution to extract the geojson in python directly
-file_name_geojson = "data/SAME/codes.geojson"
+date = "18mr25" 
+shapefile_url = f"https://www.weather.gov/source/gis/Shapefiles/County/cs{date}.zip"
 folder_name = "data/SAME/"
 
 def write_to_file(geojson):
-    print(geojson)
     path = os.path.join(os.path.dirname(__file__), folder_name, geojson['properties']['AREA_SAME'] + ".geojson")
     with open(path, 'w') as f:
         f.write(json.dumps(geojson))
+        f.close()
 
-geojson = json.loads(open(file_name_geojson, 'r').read())
-for region in geojson['features']:
+reader = shapefile.Reader(shapefile_url)
+collection = reader.shapeRecords()
+for region in collection.__geo_interface__['features']:
     write_to_file(region)
