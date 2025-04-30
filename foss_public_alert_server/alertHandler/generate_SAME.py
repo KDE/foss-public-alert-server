8772c6b4858aec6bbc8f3aca9786f8d3eb2a914e
apple cuckoo <nufjoysb@duck.com>
2025-04-30 20:45:53 +1200

    script update for marine and statewide SAME codes

--- foss_public_alert_server/alertHandler/generate_SAME.py
+++ foss_public_alert_server/alertHandler/generate_SAME.py
@@ -2,21 +2,37 @@
 # SPDX-FileCopyrightText: 2025 applecuckoo <nufjoysb@duck.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+# note: marine SAME codes are symlinked to the marine zone UGCs and as such are created by generate_UGC.py
+
 import json
 import os
 import shapefile
 
-date = "18mr25" 
-shapefile_url = f"https://www.weather.gov/source/gis/Shapefiles/County/cs{date}.zip"
-folder_name = "data/SAME/"
+DATE = '18mr25'
+COUNTY_URL = f'https://www.weather.gov/source/gis/Shapefiles/County/cs{DATE}.zip'
+STATE_URL = f'https://www.weather.gov/source/gis/Shapefiles/County/s_{DATE}.zip'
+FOLDER_NAME = 'data/SAME/'
 
-def write_to_file(geojson):
-    path = os.path.join(os.path.dirname(__file__), folder_name, geojson['properties']['AREA_SAME'] + ".geojson")
-    with open(path, 'w') as f:
+def write_to_file(geojson, name):
+    path = os.path.join(os.path.dirname(__file__), FOLDER_NAME, name + '.geojson')
+    with open(path, 'w', encoding='utf-8') as f:
         f.write(json.dumps(geojson))
         f.close()
 
-reader = shapefile.Reader(shapefile_url)
-collection = reader.shapeRecords()
-for region in collection.__geo_interface__['features']:
-    write_to_file(region)
+def return_county_same(polygon):
+    """return county SAME code"""
+    return f'{polygon['properties']['AREA_SAME']}'
+
+def return_state_same(polygon):
+    """return state SAME code"""
+    return f'0{polygon['properties']['FIPS']}000'
+
+datasets = {'county': {'url': COUNTY_URL, 'namefunc': return_county_same}, 'state': {'url': STATE_URL, 'namefunc': return_state_same}}
+
+for dataset_name, dataset_attributes in datasets.items():
+    # read url from dataset entry and create the collection object
+    reader = shapefile.Reader(dataset_attributes['url'])
+    collection = reader.shapeRecords()
+    for region in collection.__geo_interface__['features']:
+        # write every shape to a geojson file named by a function using the shape's attributes
+        write_to_file(region, dataset_attributes['namefunc'](region))
--- foss_public_alert_server/alertHandler/generate_UGC.py
+++ foss_public_alert_server/alertHandler/generate_UGC.py
@@ -4,11 +4,22 @@
 
 import json
 import os
+import csv
+from io import StringIO
+import requests
 import shapefile
 
-# note: CURRENT_DATE may need to be updated from time to time in the case of new releases with a later valid date.
+# note: these two date variables may need to be updated from time to time in the case of new releases with a later valid date.
 CURRENT_DATE = '18mr25'
-FOLDER_NAME = 'data/UGC/'
+LOOKUP_DATE = '14fe25'
+
+UGC_FOLDER_NAME = 'data/UGC/'
+SAME_FOLDER_NAME = 'data/SAME/'
+
+lookup_sequence = ['SSALPHA', 'SSNUM', 'ABBNAME', 'AREANAME']
+lookup_text = requests.get(f'https://www.weather.gov/source/gis/Shapefiles/WSOM/marst{LOOKUP_DATE}.txt', timeout=10).text # check https://www.weather.gov/gis/EasNWR for updates
+lookup_data = list(csv.DictReader(StringIO(lookup_text), fieldnames=lookup_sequence, delimiter='|', quotechar='"'))
+
 
 COASTAL_LINK = f'https://www.weather.gov/source/gis/Shapefiles/WSOM/mz{CURRENT_DATE}.zip'  # check https://www.weather.gov/gis/MarineZones for updates
 OFFSHORE_LINK = f'https://www.weather.gov/source/gis/Shapefiles/WSOM/oz{CURRENT_DATE}.zip' # check https://www.weather.gov/gis/MarineZones for updates
@@ -16,22 +27,21 @@ ZONES_LINK = f'https://www.weather.gov/source/gis/Shapefiles/WSOM/z_{CURRENT_DAT
 COUNTY_LINK = f'https://www.weather.gov/source/gis/Shapefiles/County/c_{CURRENT_DATE}.zip' # check https://www.weather.gov/gis/Counties for updates
 
 def write_to_file(geojson, name):
-    path = os.path.join(os.path.dirname(__file__), FOLDER_NAME, name + '.geojson')
+    path = os.path.join(os.path.dirname(__file__), UGC_FOLDER_NAME, name + '.geojson')
     with open(path, 'w', encoding='utf-8') as f:
         f.write(json.dumps(geojson))
         f.close()
 
-# dummy function to return complete UGC from coastal and offshore datasets
 def return_simple_id(polygon):
+    '''dummy function to return complete UGC from coastal and offshore datasets'''
     return polygon['properties']['ID']
 
-# assembles UGC in ssZnnn format, where ss is the state code and zzz is the zone number
 def return_zone_ugc(polygon):
+    '''assembles UGC in ssZnnn format, where ss is the state code and zzz is the zone number'''
     return f'{polygon['properties']['STATE']}Z{polygon['properties']['ZONE']}'
 
-# assembles UGC in ssCnnn format, where ss is the state code and nnn is the FIPS number.
-# the [2:] is to strip out the state number.
 def return_county_ugc(polygon):
+    '''assembles UGC in ssCnnn format, where ss is the state code and nnn is the FIPS number.'''
     return f'{polygon['properties']['STATE']}C{polygon['properties']['FIPS'][2:]}'
 
 datasets = {'coastal': {'url': COASTAL_LINK, 'namefunc': return_simple_id},
@@ -44,3 +54,17 @@ for dataset_name, dataset_attributes in datasets.items():
     collection = reader.shapeRecords()
     for region in collection.__geo_interface__['features']:
         write_to_file(region, dataset_attributes['namefunc'](region))
+        # symlink coastal zones to a SAME code
+        if dataset_name == 'coastal':
+            for rows in lookup_data:
+                # check the two-letter area code in the UGC against each row.
+                if region['properties']['ID'][:2] == rows['SSALPHA']:
+                    os.makedirs(SAME_FOLDER_NAME, exist_ok=True)
+                    # create a path to a file in the SAME folder with the format 0SSCCC - SS is from the marine zone lookup table while CCC comes from the UGC
+                    dest = os.path.join(SAME_FOLDER_NAME, f"0{rows['SSNUM']}{region['properties']['ID'][3:]}.geojson")
+
+                    if os.path.isfile(dest):
+                        os.remove(dest)
+
+                    src = os.path.join(os.path.relpath(UGC_FOLDER_NAME, SAME_FOLDER_NAME), f"{region['properties']['ID']}.geojson")
+                    os.symlink(src, dest)
