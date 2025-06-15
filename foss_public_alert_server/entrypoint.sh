1cd3f5f507d98d213ccdf75558651df076cfc943
Nucleus <nucleus-ffm@posteo.de>
2025-06-15 14:41:06 +0200

    add notification worker to docker entrypoint.sh

--- foss_public_alert_server/entrypoint.sh
+++ foss_public_alert_server/entrypoint.sh
@@ -5,7 +5,8 @@
 python3 manage.py collectstatic --clear --no-input
 python3 manage.py migrate
 
-celery -A foss_public_alert_server worker --loglevel=INFO &
+celery -A foss_public_alert_server worker --loglevel=INFO -n general --concurrency 4 &
+celery -A foss_public_alert_server worker --loglevel=INFO -Q push_notifications -n notifications --concurrency 1 &
 celery -A foss_public_alert_server beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 uwsgi --socket :3032 --http :8000 --uid www-data --wsgi-file /app/foss_public_alert_server/wsgi.py
