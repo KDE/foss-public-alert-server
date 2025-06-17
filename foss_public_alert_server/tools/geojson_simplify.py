6dfe7aa3ee27bb80851dc6a94e943ccf2bacd585
Volker Krause <vkrause@kde.org>
2025-06-17 17:22:24 +0200

    Add geometry simplification infrastructure
    Not applied yet to our checked in data, but already useful to validate
    the approach and determine the correct parameters. Even with the very
    defensive defaults it's achieving 10-150x reductions on the particularly
    large coastal geometries, with the resulting polygon still fully containing
    the original one.
    
    The basic idea is the following 4 steps:
    (1) Expand the polygon. This is very effective in removing bays/fjords/etc
        and "swallowing" islands.
    (2) Apply the Douglas-Peucker simplification. This reduces the level of
        detail of the polygon lines. Using a smaller threshold than use for
        expansion guarantees we still contain the original geometry.
    (3) Shrink the result again. This is to get back closer to the original
        geometry and should be done by the amount we expanded minus half the
        simplification threshold to still cover the original geometry.
    (4) Round coordinate decimals to an appropriate length. This is computed
        to be enough to represent 1/10th of the simplification threshold.
        That's still very defensive but at the same time reasonably effective.
    
    While this is currently mainly used via its own command line tool, most of
    the logic is in library code and could be directly added to the data update
    scripts if needed.

---
+++ foss_public_alert_server/tools/geojson_simplify.py
@@ -0,0 +1,83 @@
+# SPDX-FileCopyrightText: 2023 Volker Krause <vkrause@kde.org>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import argparse
+import json
+import math
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from lib import geojson
+
+
+parser = argparse.ArgumentParser(description='Simplify GeoJSON polygons for use in CAP alerts.')
+parser.add_argument('--input', type=str, required=True, help='Input GeoGJSON file to simplify')
+parser.add_argument('--output', type=str, required=True, help='File to write the simplified GeoJSON to')
+parser.add_argument('--expand', type=float, help='Ratio of the overall size to expand the polygon by', default=0.005)
+parser.add_argument('--threshold', type=float, help='Ratio of the overall size to use as Douglas-Peucker simplification threshold', default=0.0025)
+parser.add_argument('--shrink', type=float, help='Ratio of the overall size to shrink the polygon by', default=0.004)
+arguments = parser.parse_args()
+
+
+def simplify_multi_polygon(multi_poly):
+    """
+    Simplify a GeoJSON multi-polygon, using the following approach:
+    - Drop inner rings (not supported by CAP)
+    - Offset the outer ring by 0.5%. This makes narrow concave parts disappear (such as bays, fjords, etc)
+      and it ensures that we still cover the original area despite the following step.
+    - Apply the Douglas-Peucker algorithm to remove points within a 0.25% threshold.
+    - Offset the result by -0.4%. This is to get back closer to the original coverage area.
+    - Round GEOJSON coordinates to avoid floating point number serialization bloat.
+    """
+
+    # drop inner rings
+    # CAP doesn't support polygons with cutouts, so drop those here
+    for poly in multi_poly:
+        del poly[1:]
+
+    # determine the size of the covered area
+    # we use that to adjust all parameters for the following relative to the size
+    # of the covered area, ie. larger areas will be simplified with more aggressive parameters.
+    bbox = geojson.geojson_multipolygon_bounding_box(multi_poly)
+    lat_center = (bbox[0][1] + bbox[1][1]) / 2.0
+    bbox_width = round(geojson.geojson_distance([bbox[0][0], lat_center], [bbox[1][0], lat_center]), 0)
+    bbox_height = round(geojson.geojson_distance(bbox[0], [bbox[0][0], bbox[1][1]]), 0)
+    dist = round(max(bbox_width, bbox_height))
+    print(f"Area size: {bbox_width} x {bbox_height}m")
+
+    if arguments.expand < arguments.shrink:
+        print("WARNING: shrinking more than expanding, result will likely not include the original area!")
+    offset1 = dist * arguments.expand
+    threshold = offset1 * arguments.threshold
+    offset2 = dist * arguments.shrink
+
+    # use enough decimals to fit 1/10th of the simplification threshold
+    coord_per_meter_lon = (bbox[1][0] - bbox[0][0]) / bbox_width
+    coord_per_meter_lat = (bbox[1][1] - bbox[0][1]) / bbox_height
+    decimals = int(-math.log(min(coord_per_meter_lat, coord_per_meter_lon) * threshold * 0.1, 10)) + 1
+
+    print(f"Applying offset: {offset1}m…")
+    r = geojson.geojson_multipolygon_offset(multi_poly, offset1)
+    print(f"Applying Douglas-Peucker simplification with threshold {threshold}m…")
+    for ring in r:
+        ring[0] = geojson.geojson_ring_simplify(ring[0], threshold)
+    print(f"Applying negative offset: {offset2}m…")
+    r = geojson.geojson_multipolygon_offset(r, -offset2)
+    print(f"Rounding to {decimals} decimals…")
+    r = geojson.geojson_multipolygon_round_coordinates(r, decimals)
+    return r
+
+
+# load GeoJSON input
+with open(arguments.input, 'r') as f:
+    g = json.load(f)
+
+if g['geometry']['type'] == 'MultiPolygon':
+    g['geometry']['coordinates'] = simplify_multi_polygon(g['geometry']['coordinates'])
+elif g['geometry']['type'] == 'Polygon':
+    g['geometry']['coordinates'] = simplify_multi_polygon([g['geometry']['coordinates']])[0]
+
+with open(arguments.output, 'w') as f:
+    json.dump(g, f)
