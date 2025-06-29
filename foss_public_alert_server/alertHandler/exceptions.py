460cd20a7709a49d18c21b46a162ebf19eee7546
Nucleus <nucleus-ffm@posteo.de>
2025-06-29 11:08:27 +0200

    Use Exception instead of BaseException as this is recommended by Python, and BaseException causes issues with celery workers

--- foss_public_alert_server/alertHandler/exceptions.py
+++ foss_public_alert_server/alertHandler/exceptions.py
@@ -1,29 +1,29 @@
 # SPDX-FileCopyrightText: Nucleus <nucleus-ffm@posteo.de>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-class AlertExpiredException(BaseException):
+class AlertExpiredException(Exception):
     pass
 
 
-class DatabaseWritingException(BaseException):
+class DatabaseWritingException(Exception):
     """
     raised if something went wrong while writing in the database
     """
     pass
 
 
-class AlertParameterException(BaseException):
+class AlertParameterException(Exception):
     pass
 
 
-class NoGeographicDataAvailableException(BaseException):
+class NoGeographicDataAvailableException(Exception):
     """
     This exception is raised if we do not have geo information for this alert. This can be due to missing geocode
     translation
     """
     pass
 
-class NothingChangedException(BaseException):
+class NothingChangedException(Exception):
     """
     This exception is raised if a http request returns status code 304  Nothing changed
     """
--- foss_public_alert_server/subscriptionHandler/exceptions.py
+++ foss_public_alert_server/subscriptionHandler/exceptions.py
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: Nucleus <nucleus-ffm@posteo.de>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-class PushNotificationCheckFailed(BaseException):
+class PushNotificationCheckFailed(Exception):
     pass
 
-class PushNotificationException(BaseException):
+class PushNotificationException(Exception):
     pass
