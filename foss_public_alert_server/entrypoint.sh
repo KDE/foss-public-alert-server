a6bbdf1e910e10f551bb9cdfddef94a4c9c8fe60
Nucleus <nucleus-ffm@posteo.de>
2026-04-28 12:45:45 +0200

    Migrate to UV and update dependencies and readme
    python version to 3.12
    Django to >=6.0 <7.0
    kombu to 5.6.2

--- foss_public_alert_server/entrypoint.sh
+++ foss_public_alert_server/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2022 Volker Krause <vkrause@kde.org>
+# SPDX-FileCopyrightText: Nucleus <nucleus-ffm@posteo.de>
 # SPDX-License-Identifier: CC0-1.0
 
 rm -rf /tmp/fpas-metrics
 mkdir -p /tmp/fpas-metrics
 export PROMETHEUS_MULTIPROC_DIR=/tmp/fpas-metrics
 
-python3 manage.py collectstatic --clear --no-input
-python3 manage.py migrate
+uv run manage.py collectstatic --clear --no-input
+uv run manage.py migrate
 
 celery -A foss_public_alert_server worker --loglevel=INFO -n general --concurrency 4 &
 celery -A foss_public_alert_server worker --loglevel=INFO -Q push_notifications -n notifications --concurrency 1 &
